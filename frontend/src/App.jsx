177f05c3468cf371a310ec35ab76a5c898ffd350
Anne Kurvinen <kurvinen@live.se>
2024-11-05 20:49:16 +0100

    modified: package-lock.json         modified:   package.json         modified:   src/App.jsx         deleted:    src/server.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'; 
 
 function App() {
@@ -8,31 +8,44 @@ function App() {
     setDropdownOpen(!dropdownOpen);
   };
 
+  useEffect(() => {
+    fetch('/api')
+      .then((response) => response.json())
+      .then((result) => {
+        alert(`Hello ${result.hello}!`)
+      })
+  }, [])
+
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-container">
           <ul className="navbar-list">
             <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/services">Services</a></li>
-            <li><a href="/blog">Logga In</a></li>
+            <li><a href="/om-oss">Öppettider</a></li>
+            <li><a href="/services">Services</a></li>  
+            <li><a href="/inlog">Logga In</a></li>       
             <li className="dropdown">
               <button className="dropdown-btn" onClick={toggleDropdown}>
                 Bli Medlem
               </button>
               {dropdownOpen && (
                 <ul className="dropdown-menu">
-                  <li><a href="/contact">Registrering</a></li>
-                  <li><a href="/faq">Info - Medlemskap</a></li>
+                  <li><a href="/registrering">Registrering</a></li>
+                  <li><a href="/info">Info - Medlemskap</a></li>
                 </ul>
               )}
             </li>
           </ul>
         </div>
       </nav>
+
+            {/* Resten av din app-innehåll här */}
+            <div>
+        <h1>VÄLKOMMEN TILL VÅRT TRÄNINGSCENTER!</h1>
+        <p>This is the content below the sticky navbar.</p>
+      </div>
     </div>
   );
 }
-
 export default App;
