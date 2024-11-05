484dcb9fc9054a3fed888e6e08d66785bfb1a94b
Anne Kurvinen <kurvinen@live.se>
2024-11-05 20:47:16 +0100

    backend modified:   index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
-const express = require('express')
+const express = require('express'),
+  path = require('path')
 
-const app = express(),
-  port = process.env.PORT || 3000
+const app = express()
 
-  app.get('/', (_request, response) => {
+const port = process.env.PORT || 3000;
+
+app.get('/api', (_request, response) => {
   response.send({ hello: 'World' })
 })
 
+app.use(express.static(path.join(path.resolve(), 'dist')))
+
 app.listen(port, () => {
-  console.log(`Redo på http://localhost:${port}/`)
-})
+    console.log(`Server running on http://localhost:${port}`);
+});
