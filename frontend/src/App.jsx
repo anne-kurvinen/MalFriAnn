00f48f7c15d984ba914fc2ea0f3556841176e9ab
Frida Ericsson <fridaericsson@live.com>
2024-11-05 22:55:30 +0100

    merge conflicts and changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+
 
 function App() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
