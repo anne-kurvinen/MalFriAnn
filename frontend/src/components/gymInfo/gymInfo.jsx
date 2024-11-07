c6112d64fb1bf5084f056b76049c457025883548
Frida Ericsson <fridaericsson@live.com>
2024-11-07 13:23:05 +0100

    changes to text in gymInfo

--- frontend/src/components/gymInfo/gymInfo.jsx
+++ frontend/src/components/gymInfo/gymInfo.jsx
@@ -5,7 +5,7 @@ function GymInfo() {
   return (
  <div className="info-container">
     <h2>En träningsupplevelse utöver det vanliga</h2>
-    <p>På vårt nya gym tar vi hand om alla detaljer, så att du kan fokusera fullt ut på din träning. Med ett professionellt team och moderna tjänster ser vi till att ditt besök hos oss blir både smidigt och inspirerande. Vi har skapat en miljö där kvalitet och komfort står i centrum för din utveckling</p>
+    <p>På vårt gym tar vi hand om alla detaljer, så att du kan fokusera fullt ut på din träning. Med ett professionellt team och moderna tjänster ser vi till att ditt besök hos oss blir både smidigt och inspirerande. Vi har skapat en miljö där kvalitet och komfort står i centrum för din utveckling</p>
     <p>
         <a href="/login" className="link">Logga in</a> eller <a href="/register" className="link">bli medlem</a> för att ta del av våra tjänster.
       </p> 
