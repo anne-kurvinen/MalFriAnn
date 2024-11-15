6f81e80b2d68ce90d04cdd8ab603aebf7a414dd4
Anne Kurvinen <kurvinen@live.se>
2024-11-15 12:40:14 +0100

    Skapat Logga ut knapp samt döljt andra knappar. Uppdaterat Ändra & Radera profil knapparna

--- frontend/src/components/navbar/navbarComponent.jsx
+++ frontend/src/components/navbar/navbarComponent.jsx
@@ -21,6 +21,11 @@ function NavbarComponent() {
     closeLoginModal();
   };
 
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    // Lägg till eventuell ytterligare utloggningslogik här, t.ex. rensa token från localStorage
+  };
+
   return (
     <div>
       <nav className="navbar">
@@ -29,19 +34,24 @@ function NavbarComponent() {
             <li><Link to="/"><i className="fas fa-home"></i> Home</Link></li>
             <li><Link to="/about"><i className="fas fa-clock"></i> Öppettider</Link></li>
             <li><Link to="/settings"><i className="fas fa-cog"></i> Inställningar</Link></li>  
-            <li><button className="loginbtn" onClick={openLoginModal}><i className="fas fa-sign-in-alt"></i> Logga In</button></li>       
-            <li className="dropdown">
-              <button className="dropbtn"><i className="fas fa-user-plus"></i> Bli Medlem </button>
-              <ul className="dropdown-content">
-                <li><Link to="/registration"><i className="fas fa-user-edit"></i> Registrering</Link></li>
-                <li><Link to="/gyminfo"><i className="fas fa-info-circle"></i> Info - Medlemskap</Link></li>
-              </ul>
-            </li>
+            {!isLoggedIn && (
+              <>
+                <li><button className="loginbtn" onClick={openLoginModal}><i className="fas fa-sign-in-alt"></i> Logga In</button></li>
+                <li className="dropdown">
+                  <button className="dropbtn"><i className="fas fa-user-plus"></i> Bli Medlem </button>
+                  <ul className="dropdown-content">
+                    <li><Link to="/registration"><i className="fas fa-user-edit"></i> Registrering</Link></li>
+                    <li><Link to="/gyminfo"><i className="fas fa-info-circle"></i> Info - Medlemskap</Link></li>
+                  </ul>
+                </li>
+              </>
+            )}
             {isLoggedIn && (
-              <ul>
-              <li><Link to="/myaccount"><i className="fas fa-user"></i> My Account</Link></li>
-              <li><Link to="/notes"><i className="fas fa-notes"></i> My notes</Link></li>
-              </ul>
+              <>
+                <li><Link to="/myaccount"><i className="fas fa-user"></i> My Account</Link></li>
+                <li><Link to="/notes"><i className="fas fa-notes"></i> My notes</Link></li>
+                <li><button className="logoutbtn" onClick={handleLogout}><i className="fas fa-sign-out-alt"></i> Logga Ut</button></li>
+              </>
             )}
           </ul>
         </div>
